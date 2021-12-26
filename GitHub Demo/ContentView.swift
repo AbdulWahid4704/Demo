56f83bc92cf13054461784ceacdd9d87688c034e
abdul wahid <abdulwahith4704@gmail.com>
2021-12-26 14:35:15 +0400

    Initial Commit

---
+++ GitHub Demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GitHub Demo
+//
+// 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
+//
+//  Created by Rusaab Abdul Wahid on 26/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
